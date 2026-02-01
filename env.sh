02b085008f0f15e571a5e9d183b86d5dbf0e08a1
zejunchen-zejun <zejun.chen@amd.com>
2026-02-01 22:00:52 +0800

    add
    Signed-off-by: zejunchen-zejun <zejun.chen@amd.com>

--- env.sh
+++ env.sh
@@ -1,16 +1,11 @@
-# alias caa='cd /home/zejchen/plugin/ATOM/test_plugin'
-# alias cvv='cd /home/zejchen/rocm_vllm/vllm'
-# alias css='cd /home/zejchen/zejun_sglang/sglang'
-alias caa='cd /home/zejchen@amd.com/plugin/ATOM/test_plugin'
+alias caa='cd /home/zejchen@amd.com/plugin/ATOM/ATOMModelBackendValidationHelper'
 
 alias gg='git fetch && git checkout origin/zejun/plugin_for_atom_1223'
 alias ggg='git fetch && git checkout origin/zejun/model_impl'
-# alias gb='git checkout 1ce085879dd473dae397cabbf57a4a3240530a61'
 alias gbb='git checkout origin/zejun/plugin_for_atom_1223'
 
 alias tt0='bash ./test.0.6B.tp1.no.custom.sh'
 alias tt1='bash ./test.0.6B.tp1.sh'
-# alias tt2='bash ./test.235B.tp4.sh'
 alias tt3='bash ./test.235B.tp8.ep8.sh'
 alias tt4='bash ./test.235B.tp8.ep8.original.sh'
 
