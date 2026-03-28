c2a89d3761e4b8bb33b1eaa789fa70a14e8328df
zejunchen-zejun <zejun.chen@amd.com>
2026-03-28 15:49:01 +0800

    add
    Signed-off-by: zejunchen-zejun <zejun.chen@amd.com>

--- atom/curl.sh
+++ atom/curl.sh
@@ -5,7 +5,7 @@ curl -X POST "http://localhost:8000/start_profile"
 curl -X POST "http://localhost:8000/v1/completions" \
     -H "Content-Type: application/json" \
     -d '{
-        "prompt": "The capital of China is", "temperature": 0, "top_p": 1, "top_k": 0, "max_tokens": 64, "stream": false, "ignore_eos": false, "seed": 123
+        "prompt": "The capital of China is", "temperature": 0, "top_p": 1, "top_k": -1, "max_tokens": 64, "stream": false, "ignore_eos": false, "seed": 123
 }'
 
 curl -X POST "http://localhost:8000/stop_profile"
